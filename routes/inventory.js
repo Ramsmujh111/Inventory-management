dedba5fc858f69707cd81ad0b831d9cc69090af5
Ramsmujh <dev.bit.ram@gmail.com>
2023-06-29 20:01:48 +0530

    remove comment

--- routes/inventory.js
+++ routes/inventory.js
@@ -3,5 +3,4 @@ const routes = express.Router();
 const ControllerInventory = require('../controller/Invetory');
 
 routes.post('/api/post' , ControllerInventory.PostApiCall);
-routes.post('/' , ControllerInventory.add)
 module.exports = routes;
